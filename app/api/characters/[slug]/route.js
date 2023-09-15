a7484ab60ffac1eb9ea6b4f0d68619caace83aa1
Rifkiilmi <rifkiilmi98@gmail.com>
2023-09-15 14:23:37 +0700

    first commit

--- app/api/characters/[slug]/route.js
+++ app/api/characters/[slug]/route.js
@@ -12,6 +12,23 @@ import characters from '@/data/characters.json'
 import qoutes from '@/data/qoutes.json'
 import { NextResponse } from 'next/server'
 
-export async function GET() {
-  return NextResponse.json({})
+export async function GET(req, { params }) {
+  try {
+    const character = characters.data.find(item => item.slug === params.slug)
+
+    if (!character) {
+      return new NextResponse('not found', { status: 404 })
+    }
+
+    const character_quotes = qoutes.data.filter(
+      item => item.character_id === character.id,
+    )
+
+    return NextResponse.json({
+      character,
+      character_quotes: character_quotes.length > 0 ? character_quotes : null,
+    })
+  } catch (error) {
+    return new NextResponse('Internal Server Error', { status: 500 })
+  }
 }
